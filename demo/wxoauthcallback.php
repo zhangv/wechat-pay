badf1c1414a71b080aada7795541d6c8d8ed8c07
zhangv <derekzhangv@gmail.com>
2018-01-02 10:36:25 +0800

    fix oauthcallback

--- demo/wxoauthcallback.php
+++ demo/wxoauthcallback.php
@@ -1,9 +1,11 @@
 <?php
 
+require_once __DIR__ ."/../WechatPay.php";
+
 if (isset($_GET['code'])){
 	$code = trim($_GET['code']);
 	$state = trim($_GET['state']);
-	$at = $this->getOauthAccessToken($code);
+	$at = getOauthAccessToken($code);
 	if(!$at || empty($at->openid)){
 		die('授权失败');
 	}else{
@@ -13,4 +15,18 @@ if (isset($_GET['code'])){
 	}
 }else{
 	die('授权失败，请重试。');
+}
+
+function getOauthAccessToken($code){
+	$cfg = include './config.php';
+	$appid = $cfg['appid'];
+	$appsecret = $cfg['appsecret'];
+	$url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$appsecret&code=$code&grant_type=authorization_code";
+	$ch=curl_init($url);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+	curl_setopt($ch,CURLOPT_BINARYTRANSFER,true);
+	$output=curl_exec($ch);
+	curl_close($ch);
+	$r = json_decode($output);
+	return $r;
 }
