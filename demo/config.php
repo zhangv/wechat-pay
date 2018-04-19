d28d2e613bc9eef8f12e1fd9cfc8ed529d2b41ed
zhangv <derekzhangv@gmail.com>
2018-04-19 17:10:35 +0800

    fix config

--- demo/config.php
+++ demo/config.php
@@ -5,14 +5,14 @@ return [
 	'app_id' => 'XXXXXXXXXXXXXXXXXXX', //APPID
 	'app_secret' => 'XXXXXXXXXXXXXXXXXXXXXXXXX', //App Secret
 	'api_key' =>'XXXXXXXXXXXXXXXXXXXXXXX', //支付密钥
-	'ssl_cert_path' => __DIR__ . '/keys/apiclient_cert.pem',
-	'ssl_key_path' => __DIR__ .'/keys/apiclient_key.pem',
+	'ssl_cert_path' => __DIR__ . '/../cert/apiclient_cert.pem',
+	'ssl_key_path' => __DIR__ .'/../cert/apiclient_key.pem',
 	'sign_type' => 'MD5',
-	'notify_url' => 'http://YOURSITE/paidnotify.php',
-	'refund_notify_url' => 'http://YOURSITE/refundednotify.php',
+	'notify_url' => 'http://XXX.XXX/paidnotify.php',
+	'refund_notify_url' => 'http://XXX.XXX/refundednotify.php',
 	'h5_scene_info' => [//required in H5
 		'h5_info' => ['type' => 'Wap', 'wap_url' => 'http://wapurl', 'wap_name' => 'wapname']
 	],
-	'rsa_pubkey_path' => __DIR__ .'/keys/pubkey.pem',
+	'rsa_pubkey_path' => __DIR__ .'/../cert/pubkey.pem',
 	'jsapi_ticket' => __DIR__ .'/jsapi_ticket.json'
 ];
